f0cb29ba2ee8dd0fde2f571e02e6687246023da8
JJWallis <josh.jamesonwallis@gmail.com>
2021-10-19 13:56:42 +0100

    App ip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from './components/Header'
+import Main from './components/Main'
 
-function App() {
-   return <div></div>
+export const Context = React.createContext(null)
+
+const App: React.FC = () => {
+   const [countries, setCountries] = useState(null)
+
+   function fetchData(endpoint: string) {
+      axios
+         .get(endpoint)
+         .then((data: any) => {
+            const info = data.data
+            if (info.length > 30) {
+               setCountries(info.slice(0, 30))
+            } else {
+               setCountries(info)
+            }
+         })
+         .catch((err) => console.log(err.message))
+   }
+
+   useEffect(() => fetchData('https://restcountries.com/v3.1/all'), [])
+
+   return (
+      <Context.Provider value={countries && countries}>
+         <Header />
+         <Main />
+      </Context.Provider>
+   )
 }
 
 export default App
