e60e348c12a8a5c45d9cbb4af5c161a7ec08de4a
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-06 13:47:33 +0000

    Further details 1st draft done

--- src/components/FurtherDetails.tsx
+++ src/components/FurtherDetails.tsx
@@ -1,19 +1,41 @@
 import React, { useContext } from 'react'
 import { Context } from '../App'
 import Wrapper from './styled/Wrapper'
+import CountryImg from './styled/StyledImg'
+import { CountryName } from './styled/StyledTitle'
+import { CountrySubTitle } from './styled/CountryDataTitle'
+import { CountryData } from './styled/CountryData'
 
 const FurtherDetails: React.FC = () => {
    const context = useContext(Context)
 
-   // or 2 flex items (FurtherDetails) if false using countryDetails state from app - check if that's truthy here too?
-   // (1 fragment + wrapper with flag within + other wrapper with data)
+   const handleDisplay = () => {
+      if (context?.furtherDetails) {
+         const data: any = context.furtherDetails[0]
+         return (
+            <>
+               <Wrapper flexChild>
+                  <CountryImg src={data.flags.svg} alt="Countries flag." />
+               </Wrapper>
+               <Wrapper flexChild>
+                  <CountryName>{data.name.common}</CountryName>
+                  <Wrapper countryData>
+                     <CountrySubTitle>Population:</CountrySubTitle>
+                     <CountryData>{data.population}</CountryData>
+                  </Wrapper>
+                  <Wrapper countryData>
+                     <CountrySubTitle>Region:</CountrySubTitle>
+                     <CountryData>{data.region}</CountryData>
+                  </Wrapper>
+                  <CountrySubTitle>Capital:</CountrySubTitle>
+                  <CountryData>{data.capital}</CountryData>
+               </Wrapper>
+            </>
+         )
+      }
+   }
 
-   return (
-      <>
-         <Wrapper flexChild></Wrapper>
-         <Wrapper flexChild></Wrapper>
-      </>
-   )
+   return <>{handleDisplay()}</>
 }
 
 export default FurtherDetails
