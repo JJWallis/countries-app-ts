0bef170aef605e37f1372fd0b7cb0932986d129f
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-06 16:23:04 +0000

    FUrtherDetails refactored

--- src/components/FurtherDetails.tsx
+++ src/components/FurtherDetails.tsx
@@ -9,15 +9,24 @@ import { CountryData } from './styled/CountryData'
 const FurtherDetails: React.FC = () => {
    const context = useContext(Context)
 
-   const produceJSX = () => {}
-
    const handleContentVisible = () => {
       if (context?.furtherDetails) {
          const data: any = context.furtherDetails[0]
-         console.log(data)
          const nativeName: any = Object.values(data.name.nativeName)[0]
          const currencies: any = Object.values(data.currencies)[0]
-         const languages = Object.values(data.languages)
+         const languages = `${[...Object.values(data.languages)]}`
+
+         const dataToMap = {
+            nativeName: nativeName.official,
+            population: data.population,
+            region: data.region,
+            subregion: data.subregion,
+            capital: data.capital,
+            topLevelDomain: data.tld[0],
+            currencies: currencies.name,
+            languages: languages,
+         }
+
          return (
             <>
                <Wrapper flexChild>
@@ -25,45 +34,18 @@ const FurtherDetails: React.FC = () => {
                </Wrapper>
                <Wrapper flexChild>
                   <CountryName>{data.name.common}</CountryName>
-                  <CountryData>
-                     <CountrySubTitle>Native Name:</CountrySubTitle>
-                     {nativeName.official}
-                  </CountryData>
-                  <CountryData>
-                     <CountrySubTitle>Population:</CountrySubTitle>
-                     {data.population}
-                  </CountryData>
-                  <CountryData>
-                     <CountrySubTitle>Region:</CountrySubTitle>
-                     {data.region}
-                  </CountryData>
-                  <CountryData>
-                     <CountrySubTitle>Sub Region:</CountrySubTitle>
-                     {data.subregion}
-                  </CountryData>
-                  <CountryData>
-                     <CountrySubTitle>Capital:</CountrySubTitle>
-                     {data.capital}
-                  </CountryData>
-                  <CountryData>
-                     <CountrySubTitle>Top Level Domain:</CountrySubTitle>
-                     {data.tld[0]}
-                  </CountryData>
-                  <CountryData>
-                     <CountrySubTitle>Currencies: </CountrySubTitle>
-                     {currencies.name}
-                  </CountryData>
-                  <CountryData>
-                     <CountrySubTitle>Languages: </CountrySubTitle>
-                     {languages.map((language: any) => ` ${language},`)}
-                  </CountryData>
+                  {Object.entries(dataToMap).map(([key, value]) => (
+                     <CountryData key={key}>
+                        <CountrySubTitle>{key}:</CountrySubTitle>
+                        {value}
+                     </CountryData>
+                  ))}
                   {/* border-countries component */}
                </Wrapper>
             </>
          )
       }
    }
-
    return <>{handleContentVisible()}</>
 }
 
--- src/components/styled/CountryDataTitle.tsx
+++ src/components/styled/CountryDataTitle.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const CountrySubTitle = styled.strong`
    display: inline-block;
+   margin-right: 0.5rem;
    font-size: 1.2rem;
    color: ${(props) => props.theme.color};
 `
