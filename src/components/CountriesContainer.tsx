397ffd696693f03742b9f5f7f52e23afcf47748f
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-09 14:14:55 +0000

    CountriesContainer render func refactored

--- src/components/CountriesContainer.tsx
+++ src/components/CountriesContainer.tsx
@@ -9,37 +9,25 @@ import FurtherDetails from './FurtherDetails'
 const CountriesContainer: React.FC = () => {
    const context = useContext(Context)
 
-   // homepage state doesn't get updated - stays falsy
-   // re-factor - func that returns grid of cards (data is passed in as param - checks if fitlered data is truthy)
-   // + prioritises it if so (else default countries)
-   // split up so not double short-circuiting (check if data is truthy before + arr of components which gets returned/rendered)
    // if specifc region is already visible - don't re-render (useEffect)
+
+   const determineData = () =>
+      !context?.filteredRegions ? context?.countries : context?.filteredRegions
 
    const handleContentVisible = () => {
       if (context?.homepage) {
-         if (!context.filteredRegions) {
-            return (
-               <Wrapper display={'grid'}>
-                  {context && context.countries ? (
-                     context.countries.map((country: any) => (
-                        <CountryCard key={uuidv4()} data={country} />
-                     ))
-                  ) : (
-                     <Loading>Loading...</Loading>
-                  )}
-               </Wrapper>
-            )
-         } else {
-            return (
-               <Wrapper display={'grid'}>
-                  {context &&
-                     context.filteredRegions &&
-                     context.filteredRegions.map((country: any) => (
-                        <CountryCard key={uuidv4()} data={country} />
-                     ))}
-               </Wrapper>
-            )
-         }
+         const data = determineData()
+         return (
+            <Wrapper display={'grid'}>
+               {context && data ? (
+                  data.map((country: any) => (
+                     <CountryCard key={uuidv4()} data={country} />
+                  ))
+               ) : (
+                  <Loading>Loading...</Loading>
+               )}
+            </Wrapper>
+         )
       } else {
          return (
             <Wrapper display={'flexWrap'}>
