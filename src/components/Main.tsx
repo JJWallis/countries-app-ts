69671ef229d7366e8e45c8d1a14dc54f8b4129ee
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-11 19:11:53 +0000

    Scroll offset saving ip

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import StyledMain from './styled/StyledMain'
 import Wrapper from './styled/Wrapper'
 import SearchFilter from './SearchFilter'
 import Countries from './CountriesContainer'
+import { Context } from '../App'
 
 const Main: React.FC = () => {
+   const { homepage } = { ...useContext(Context) }
+   const main = useRef<HTMLElement | null>(null)
+
+   useEffect(() => {
+      if (main.current) {
+         if (!homepage) {
+            localStorage.setItem(
+               'scroll-offset',
+               JSON.stringify(main.current.scrollTop)
+            )
+         } else {
+            // const offset = localStorage.getItem('scroll-offset')
+            // if (offset) {
+            //    setScrollOffset(JSON.parse(offset))
+            //    main.current.scrollTop = scrollOffset
+            // }
+         }
+      }
+   }, [homepage])
+
    return (
-      <StyledMain>
+      <StyledMain ref={main}>
          <Wrapper>
             <SearchFilter />
             <Countries />
