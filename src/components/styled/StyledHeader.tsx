948365c2cafabfebccf7f77f36bfdf383bc53888
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-19 23:24:06 +0000

    Header spacing at wide done

--- src/components/styled/StyledHeader.tsx
+++ src/components/styled/StyledHeader.tsx
@@ -4,6 +4,9 @@ const StyledHeader = styled.header`
    box-shadow: ${({ theme: { headerShadow } }) => headerShadow};
    padding-block: 1.7rem;
    background-color: ${({ theme: { elements } }) => elements};
+   @media (min-width: ${({ theme }) => theme.breakpoint}) {
+      padding-block: 0;
+   }
 `
 
 export default StyledHeader
