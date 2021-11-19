58daa9f1fb26e06fb0d28008e953317affd019d9
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-19 19:27:08 +0000

    Theme Switcher styles ip

--- src/components/styled/StyledLabel.tsx
+++ src/components/styled/StyledLabel.tsx
@@ -1,10 +1,20 @@
 import styled from 'styled-components'
 
 export const StyledLabel = styled.label`
+   position: relative;
    padding-block: 1rem;
    font-size: 1.15rem;
    color: ${({ theme: { color } }) => color};
    cursor: pointer;
    &::before {
+      content: '';
+      display: block;
+      height: 10px;
+      width: 10px;
+      position: absolute;
+      left: -20px;
+      top: 50%;
+      transform: translateY(-50%);
+      background-color: black;
    }
 `
