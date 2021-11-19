8c07f45c5c9a9794a6e2bf269ecbf153934c5a1c
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-19 20:02:07 +0000

    Moon icon added

--- src/components/styled/Icon.tsx
+++ src/components/styled/Icon.tsx
@@ -6,8 +6,6 @@ type IconProps = {
 }
 
 export default styled.svg<IconProps>`
-   width: 10px;
-   height: 10px;
    ${({ moon }) =>
       moon &&
       css`
@@ -16,7 +14,7 @@ export default styled.svg<IconProps>`
          right: 100px;
          top: 50%;
          transform: translateY(-50%);
-         background-color: black;
+         height: 20px;
       `}
    ${({ search }) => search && css``}
 `
