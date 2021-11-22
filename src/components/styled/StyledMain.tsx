878eddf82ef69703904bcf5380dad84263b094ff
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-22 14:32:18 +0000

    Main bottom padding added

--- src/components/styled/StyledMain.tsx
+++ src/components/styled/StyledMain.tsx
@@ -2,5 +2,6 @@ import styled from 'styled-components'
 
 export default styled.main`
    min-height: 100vh;
+   padding-bottom: 6rem;
    background-color: ${({ theme: { mainBgColor } }) => mainBgColor};
 `
