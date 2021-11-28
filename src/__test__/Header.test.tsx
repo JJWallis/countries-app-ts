4665f5ba7616cb5763186ee0c0cce1442268c855
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-28 16:25:22 +0000

    Tests ip

--- src/__test__/Header.test.tsx
+++ src/__test__/Header.test.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Search from '../components/Search'
-
-test('Search input contains correct empty default value', () => {
-   const { getByTestId } = render(<Search />)
-   const searchInput = getByTestId('search-input')
-   expect(searchInput).toHaveValue('')
-})
 
 test('Search input correctly updates when user types', () => {
    const { getByTestId } = render(<Search />)
    const input = getByTestId('search-input')
+   expect(input).toHaveValue('')
+   fireEvent.change(input, { target: { value: 'test' } })
+   expect(input).toHaveValue('test')
 })
