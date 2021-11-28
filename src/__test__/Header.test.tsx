36019170e95c1229cc4e8659e6be1c856624956a
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-28 19:19:37 +0000

    Tests ip

--- src/__test__/Header.test.tsx
+++ src/__test__/Header.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, getQueriesForElement } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Search from '../components/Search'
-import Filter from '../components/Filter'
 
 test('search input correctly updates when user types', () => {
    const { getByTestId } = render(<Search />)
@@ -11,10 +10,4 @@ test('search input correctly updates when user types', () => {
    expect(input).toBeEnabled()
    fireEvent.change(input, { target: { value: 'test' } })
    expect(input).toHaveValue('test')
-})
-
-test('filter initally rendered correctly with valid props', () => {
-   const { getByTestId } = render(
-      <Filter prevFilter={''} updatePrevFilter={() => 'hi'} />
-   )
 })
