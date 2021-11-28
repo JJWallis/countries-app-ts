d3947ff89ab13036dee65bc5da7d0290c41f0aa6
JJWallis <josh.jamesonwallis@gmail.com>
2021-11-28 15:56:02 +0000

    Tests setup

--- src/__test__/Header.test.tsx
+++ src/__test__/Header.test.tsx
@@ -1,3 +1,10 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import Header from '../components/Header'
+
+test('Header title contains correct text', () => {
+   const { getByTestId } = render(<Header />)
+   const title = getByTestId('title')
+   expect(title).toHaveTextContent('Where in the world?')
+})
