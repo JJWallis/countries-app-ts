0c1ce5681d545b7a75cb3ead935b5cf6b3c22ee6
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-01 16:08:48 +0000

    Custom hooks ip

--- src/components/hooks/useFetch.tsx
+++ src/components/hooks/useFetch.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useEffect, useState, useRef } from 'react'
 import { data } from '../../App'
 
-export default async function useFetch(
+export default function useFetch(
    endpoint: string,
    setError: (value: React.SetStateAction<boolean>) => void
 ) {
@@ -18,7 +18,15 @@ export default async function useFetch(
             setError(true) // wont know what state to update
             console.error(err.message)
          })
+      console.log('fetching data...')
    }, [endpoint, setError])
+
+   //    only run on mount - no every other re-render?
 
    return [data, fetchError]
 }
+
+// const [data, fetchError] = useFetch(
+//    'https://restcountries.com/v3.1/al',
+//    setError
+// )
