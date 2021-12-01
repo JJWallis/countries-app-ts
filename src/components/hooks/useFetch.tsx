3b4a123fe7fb0b55ba37ac409316ee1f5f2580e0
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-01 15:37:19 +0000

    Custom hooks ip

--- src/components/hooks/useFetch.tsx
+++ src/components/hooks/useFetch.tsx
@@ -9,17 +9,15 @@ export default async function useFetch(
    const [data, setData] = useState<data>(null)
    const fetchError = useRef(false)
 
-   function fetchData(endpoint: string) {
-      axios
-         .get<data>(endpoint)
-         .then((value) => setData(value.data))
-         .catch((err) => {
-            fetchError.current = true
-            setError(true)
-            console.error(err.message)
-         })
-   }
+   axios
+      .get<data>(endpoint)
+      .then((value) => setData(value.data))
+      .catch((err) => {
+         fetchError.current = true
+         setError(true) // wont know what to update
+         console.error(err.message)
+      })
 
    //    useEffect(() => fetchData(endpoint), [endpoint])
-   return data
+   return [data, fetchError]
 }
