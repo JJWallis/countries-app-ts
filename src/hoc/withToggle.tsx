d0581c38e0040bdcb41c4b9fe7b8e01c52e2b8df
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-03 12:54:05 +0000

    Gitignore updated

--- src/hoc/withToggle.tsx
+++ src/hoc/withToggle.tsx
@@ -1,3 +1,13 @@
-import React, { ReactNode } from 'react'
+import React, { useState } from 'react'
 
-const withToggle = (Component: ReactNode) => {}
+const Toggler: React.FC = (props: any) => {
+   const [isToggled, setIsToggled] = useState(false)
+   const handleToggle = () => setIsToggled(!isToggled)
+
+   const { component: C } = props
+   return <C on={isToggled} toggleOn={handleToggle} {...props} />
+}
+
+export const withToggle = (component: React.FC) => {
+   return (props: any) => <Toggler component={component} {...props} />
+}
