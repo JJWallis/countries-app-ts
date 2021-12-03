556173395154f5f7b7109ab981c5268032a875ee
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-03 13:01:38 +0000

    HOC ip

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState, useRef } from 'react'
 import { Context } from '../components/Context'
 import Button from './styled/StyledButton'
 import Wrapper from './styled/Wrapper'
+import { withToggle } from '../hoc/withToggle'
 
 interface Props {
    prevFilter: string
