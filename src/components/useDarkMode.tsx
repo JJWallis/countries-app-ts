a0f29d2ed5a6bd73281953bd202b066834eeb8f2
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-13 22:00:58 +0000

    Dark mode hook ip

--- src/components/useDarkMode.tsx
+++ src/components/useDarkMode.tsx
@@ -7,7 +7,13 @@ export function useDarkMode() {
       () => window.matchMedia && window.matchMedia(matchDark).matches
    )
 
-   useEffect(() => {}, [])
+   useEffect(() => {
+      const matcher = window.matchMedia(matchDark)
+      const onChange = ({ matches }: { matches: boolean }) =>
+         setDarkMode(matches)
+      matcher.addListener(onChange)
+      return () => matcher.removeListener(onChange)
+   }, [setDarkMode])
 
    return [darkMode, setDarkMode]
 }
