eb3dca349a0af99e9129527b6882c8eef1726eaa
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-13 21:44:59 +0000

    Dark mode hook ip

--- src/components/useDarkMode.tsx
+++ src/components/useDarkMode.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
+const matchDark = '(prefers-color-scheme: dark)'
+
 export function useDarkMode() {
-   const [darkMode, setDarkMode] = useState(() => {})
+   const [darkMode, setDarkMode] = useState(
+      () => window.matchMedia && window.matchMedia(matchDark).matches
+   )
 
    useEffect(() => {}, [])
 
