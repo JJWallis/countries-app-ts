abbb01be01bc8581d8d531fb495845eead6e1e7e
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-03 17:39:14 +0000

    Icons refactored

--- src/components/styled/Icon.tsx
+++ src/components/styled/Icon.tsx
@@ -6,7 +6,13 @@ type IconProps = {
    arrow?: boolean
 }
 
-export default styled.svg<IconProps>`
+const SVG = styled.svg.attrs({
+   version: '1.1',
+   xmlns: 'http://www.w3.org/2000/svg',
+   xmlnsXlink: 'http://www.w3.org/1999/xlink',
+})``
+
+export default styled(SVG)<IconProps>`
    display: block;
    position: absolute;
    cursor: pointer;
