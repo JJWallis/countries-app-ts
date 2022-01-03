71189fbeb2e2904b515f1a5eeb9ec4f7628fb691
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-03 18:13:50 +0000

    useToggle hook ip

---
+++ src/hooks/useToggle.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+export const useToggle = (initialState?: boolean) => {
+   const [isToggled, setIsToggled] = useState(initialState ?? false)
+
+   const handleChange = () => setIsToggled((prevValue) => !prevValue)
+
+   return [isToggled, handleChange]
+}
