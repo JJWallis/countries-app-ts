b5c4858e0ac16b29e6f4dd69ea341a53eb2aed8c
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-03 12:05:43 +0000

    Theme object refactored - input

--- src/components/styled/StyledLabel.tsx
+++ src/components/styled/StyledLabel.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const StyledLabel = styled.label`
    padding-block: 1rem;
    font-size: 1.15rem;
-   color: ${({ theme: { color } }) => color};
+   color: ${({ theme: { dynamicTheme } }) => dynamicTheme.color};
    cursor: pointer;
-   transition: color ${({ theme: { themeTransition } }) => themeTransition};
+   transition: color ${({ theme: { baseTheme } }) => baseTheme.themeTransition};
 `
