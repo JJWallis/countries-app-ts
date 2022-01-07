d600f62d5aa12bc3b91acf48e601f257618ff1f3
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-07 13:57:30 +0000

    Wrapper props removed

--- src/components/styled/Wrapper.tsx
+++ src/components/styled/Wrapper.tsx
@@ -1,28 +1,8 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 interface Props {
-   align?: boolean
-   sf?: boolean
-   filter?: string
-   country?: boolean
-   countryImgFlag?: string
-   flexChild?: boolean
-   main?: boolean
-   bordersParent?: boolean
-   furtherColumns?: boolean
-   furtherColumnsChild?: boolean
-   display?: 'grid' | 'flexWrap' | string
-   dropDown?: boolean
-   fetchError?: boolean
-   // dynamic
    margin?: string
    padding?: string
-   flexWrap?: 'wrap' | 'no-wrap'
-   justifyContent?: string
-   alignItems?: string
-   gap?: string
-   position?: string
-   opacity?: number
 }
 
 export default styled.div.attrs<Props>(({ margin, padding }) => ({
