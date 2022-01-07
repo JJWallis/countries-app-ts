dc5e24a48b62b4155214ba7f3ed7ab1a5ca61165
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-07 13:32:11 +0000

    FilterContainer prop removed from Wrapper

--- src/components/styled/Wrapper.tsx
+++ src/components/styled/Wrapper.tsx
@@ -84,49 +84,6 @@ export default styled.div.attrs<Props>(
          }
       `}
 
-      ${({ filter }) =>
-      filter &&
-      css`
-         display: flex;
-         flex-direction: column;
-         align-items: flex-start;
-         justify-content: flex-start;
-         width: 240px;
-         padding: 1.2rem 1.7rem;
-         border-radius: 5px;
-         box-shadow: ${({ theme }) => theme.shadow};
-         background-color: ${({ theme: { elements } }) => elements};
-         cursor: pointer;
-         pointer-events: ${({ fetchError }) => (fetchError ? 'none' : 'all')};
-         &::after {
-            content: '';
-            display: block;
-            width: 0;
-            height: 0;
-            border-right: 5px solid transparent;
-            border-left: 5px solid transparent;
-            border-top: 5px solid ${({ theme: { color } }) => color};
-            position: absolute;
-            top: 153px;
-            left: 200px;
-            transition: border-top
-               ${({ theme: { themeTransition } }) => themeTransition};
-            @media (min-width: ${({ theme }) => theme.breakpoint}) {
-               left: unset;
-               right: 25px;
-               top: 50%;
-               transform: translateY(-50%);
-            }
-         }
-         transition: background-color
-               ${({ theme: { themeTransition } }) => themeTransition},
-            box-shadow ${({ theme: { themeTransition } }) => themeTransition};
-         @media (min-width: ${({ theme }) => theme.breakpoint}) {
-            justify-content: flex-end;
-            width: 220px;
-         }
-      `}
-
       ${({ dropDown }) =>
       dropDown &&
       css`
