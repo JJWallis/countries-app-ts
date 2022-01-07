210d68b2b47da9a4fb34df0863e70710983e5ed2
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-07 13:18:22 +0000

    FurtherColumns container ip

--- src/components/containers/FurtherDetailsChild.styled.tsx
+++ src/components/containers/FurtherDetailsChild.styled.tsx
@@ -6,3 +6,13 @@ export const FurtherDetailsChild = styled.div`
    justify-content: center;
    flex: 1 100%;
 `
+
+export const FurtherDetailsColumns = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   align-items: baseline;
+   gap: 3.1rem;
+   @media (min-width: ${({ theme }) => theme.furtherBreakpoint}) {
+      gap: 6rem;
+   }
+`
