5d49a2af14b74113c02059787201f82e24952095
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-10 09:00:57 +0000

    Browser Router setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import { CountriesProvider } from './context/countriesContext'
 
 ReactDOM.render(
    <React.StrictMode>
-      <CountriesProvider>
-         <App />
-      </CountriesProvider>
+      <BrowserRouter>
+         <CountriesProvider>
+            <App />
+         </CountriesProvider>
+      </BrowserRouter>
    </React.StrictMode>,
    document.getElementById('root')
 )
