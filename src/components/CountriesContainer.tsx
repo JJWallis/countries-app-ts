352ced7f8f1ac39ac9e5212da95c17c87ea3f4a3
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-10 12:29:42 +0000

    CountriesContainer working - pre back btn linking to homepage

--- src/components/CountriesContainer.tsx
+++ src/components/CountriesContainer.tsx
@@ -1,48 +1,10 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { useCountriesContext } from '../hooks/useCountriesContext'
-import { useFurtherDetailsContext } from '../hooks/useFurtherDetailsContext'
-import { useGlobalContext } from '../hooks/useGlobalContext'
-import { v4 as uuidv4 } from 'uuid'
-import { CountryContainer as Countries } from './containers/CountryContainer.styled'
 import { FurtherDetailsParent } from './containers/FurtherDetailsContainers.styled'
-import { Loading } from './styled/StyledTitle'
 import HomeMain from '../routes/HomeMain'
-import CountryCard from './CountryCard'
 import FurtherDetails from './FurtherDetails'
 
 const CountriesContainer: React.FC = () => {
-   // const { filteredRegions } = useGlobalContext()
-   // const { countries, countriesError } = useCountriesContext()
-   // const { furtherDetails } = useFurtherDetailsContext()
-
-   // const determineData = () => (!filteredRegions ? countries : filteredRegions)
-
-   // const handleContentVisible = () => {
-   //    if (!furtherDetails) {
-   //       const data = determineData()
-   //       return data ? (
-   //          <Countries>
-   //             {data.map((country) => (
-   //                <CountryCard key={uuidv4()} data={country} />
-   //             ))}
-   //          </Countries>
-   //       ) : (
-   //          <Loading>
-   //             {countriesError
-   //                ? 'Country data could not be retrieved. Please reload & try again.'
-   //                : 'Loading...'}
-   //          </Loading>
-   //       )
-   //    }
-
-   //    return (
-   //       <FurtherDetailsParent>
-   //          <FurtherDetails />
-   //       </FurtherDetailsParent>
-   //    )
-   // }
-
    return (
       <>
          <Routes>
