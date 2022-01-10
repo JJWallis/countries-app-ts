cc60e0c4a1aa6ce4b9136bca83782fa9eec0002d
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-10 12:47:17 +0000

    SearchFilter layout bug fixed

--- src/components/SearchFilter.tsx
+++ src/components/SearchFilter.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { Link, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { SearchFilterContainer } from './containers/SearchFilterContainer.styled'
 import HomeHeader from '../routes/HomeHeader'
 import FurtherHeader from '../routes/FurtherHeader'
 
 const SearchFilter: React.FC = () => {
    return (
-      <>
-         <Link to="/">home</Link>
-         <Link to="/details">details</Link>
-
+      <SearchFilterContainer>
          <Routes>
             <Route path="/" element={<HomeHeader />} />
             <Route path="/details" element={<FurtherHeader />} />
-            {/* inline component rendering to fix CSS bugs? */}
          </Routes>
-      </>
+      </SearchFilterContainer>
    )
 }
 
