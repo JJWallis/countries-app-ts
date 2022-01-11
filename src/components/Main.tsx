c3b2f42ba2297a2a3d65de9061a7947825b565a5
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-11 18:40:08 +0000

    FilteredRegions housed in Main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,28 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import StyledMain from './styled/StyledMain'
 import { MainContainer } from './containers/MainContainer.styled'
 import SearchFilter from './SearchFilter'
 import Countries from './CountriesContainer'
+import { CountryData } from '../types/countriesContext.interface'
+import { useCountriesContext } from '../hooks/useCountriesContext'
+import { GlobalContext } from '../context/filteredRegionsContext'
 
 const Main: React.FC = () => {
-   // const [filteredRegions, setFilteredRegions] = useState<CountryData>(null)
+   const [filteredRegions, setFilteredRegions] = useState<CountryData>(null)
+   const { countries } = useCountriesContext()
 
-   // const handleFilterRegions = (region: string) => {
-   //    if (!region) {
-   //       setFilteredRegions(null)
-   //       return
-   //    }
-   //    const filteredData = countries?.filter(
-   //       (country) => country.region.toLowerCase() === region.toLowerCase()
-   //    )
-   //    filteredData && setFilteredRegions(filteredData)
-   // }
+   const handleFilterRegions = (region: string) => {
+      if (!region) {
+         setFilteredRegions(null)
+         return
+      }
+      const filteredData = countries?.filter(
+         (country) => country.region.toLowerCase() === region.toLowerCase()
+      )
+      filteredData && setFilteredRegions(filteredData)
+   }
 
    return (
       <StyledMain>
          <MainContainer>
-            <SearchFilter />
-            <Countries />
+            <GlobalContext.Provider
+               value={{
+                  filteredRegions,
+                  handleFilterRegions,
+               }}
+            >
+               <SearchFilter />
+               <Countries />
+            </GlobalContext.Provider>
          </MainContainer>
       </StyledMain>
    )
