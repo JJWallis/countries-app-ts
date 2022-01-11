2942925b3b013dbbdfc6005f7b26b642300948e8
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-11 09:49:32 +0000

    ErrorMsg working

--- src/components/CountriesContainer.tsx
+++ src/components/CountriesContainer.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { FurtherDetailsParent } from './containers/FurtherDetailsContainers.styled'
 import { ErrorMsg } from './styled/ErrorMsg.styled'
+import { SearchFilterContainer } from './containers/SearchFilterContainer.styled'
 import HomeMain from '../routes/HomeMain'
 import FurtherDetails from './FurtherDetails'
-import { SearchFilterContainer } from './containers/SearchFilterContainer.styled'
 import BackButton from './BackButton'
 
 const CountriesContainer: React.FC = () => {
@@ -29,7 +29,7 @@ const CountriesContainer: React.FC = () => {
                      </SearchFilterContainer>
                      <ErrorMsg>
                         404
-                        <br /> please return to homepage
+                        <br /> Please return to homepage
                      </ErrorMsg>
                   </>
                }
