8bd3edb193761847b0b5c1175e0a08699e975243
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-11 18:32:10 +0000

    new FurtherDetails context comments added - housed in Main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,6 +5,19 @@ import SearchFilter from './SearchFilter'
 import Countries from './CountriesContainer'
 
 const Main: React.FC = () => {
+   // const [filteredRegions, setFilteredRegions] = useState<CountryData>(null)
+
+   // const handleFilterRegions = (region: string) => {
+   //    if (!region) {
+   //       setFilteredRegions(null)
+   //       return
+   //    }
+   //    const filteredData = countries?.filter(
+   //       (country) => country.region.toLowerCase() === region.toLowerCase()
+   //    )
+   //    filteredData && setFilteredRegions(filteredData)
+   // }
+
    return (
       <StyledMain>
          <MainContainer>
