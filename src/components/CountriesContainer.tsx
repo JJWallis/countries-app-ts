e26ba28de973965e4d153c3d70432bcee7fa5d54
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-11 09:39:41 +0000

    BackBtn component ip

--- src/components/CountriesContainer.tsx
+++ src/components/CountriesContainer.tsx
@@ -4,6 +4,10 @@ import { FurtherDetailsParent } from './containers/FurtherDetailsContainers.styl
 import { ErrorMsg } from './styled/ErrorMsg.styled'
 import HomeMain from '../routes/HomeMain'
 import FurtherDetails from './FurtherDetails'
+import { Link } from 'react-router-dom'
+import Button from '../components/styled/StyledButton'
+import Icon from '../components/styled/Icon'
+import { SearchFilterContainer } from './containers/SearchFilterContainer.styled'
 
 const CountriesContainer: React.FC = () => {
    return (
@@ -21,7 +25,25 @@ const CountriesContainer: React.FC = () => {
             <Route
                path="*"
                element={
-                  <ErrorMsg>404 error - this page doesn't exist</ErrorMsg>
+                  <>
+                     <SearchFilterContainer>
+                        <Link to="/">
+                           <Button back>Back</Button>
+                           <Icon
+                              arrow
+                              aria-hidden="true"
+                              focusable="false"
+                              viewBox="0 0 448 512"
+                           >
+                              <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
+                           </Icon>
+                        </Link>
+                     </SearchFilterContainer>
+                     <ErrorMsg>
+                        404
+                        <br /> please return to homepage
+                     </ErrorMsg>
+                  </>
                }
             />
          </Routes>
--- src/components/styled/ErrorMsg.styled.tsx
+++ src/components/styled/ErrorMsg.styled.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
 import { Loading } from './StyledTitle'
 
-export const ErrorMsg = styled(Loading)``
+export const ErrorMsg = styled(Loading)`
+   line-height: 1.3;
+`
