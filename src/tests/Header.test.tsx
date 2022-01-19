530109e15a93d144d6b663c8a7425a7ae85a8170
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-19 15:03:28 +0000

    Header test ip

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import Header from '../components/Header'
-import { darkTheme, lightTheme } from '../components/styled/Theme'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 test('header title renders with correct text', () => {
    const { getByTestId } = render(
-      <Header
-         handleThemeChange={(dark: boolean) =>
-            dark ? setTheme(darkTheme) : setTheme(lightTheme)
-         }
-      />
+      <Header handleThemeChange={(dark: boolean) => undefined} />
    )
    const title = getByTestId('header-title')
    expect(title).toHaveTextContent('Where in the world?')
