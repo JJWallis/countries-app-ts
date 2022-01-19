4576e2ca57c362cdf0ecfa80a351d8b3a5ca6134
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-19 15:37:30 +0000

    Search test ip - router bug fix ip

--- src/tests/Search.test.tsx
+++ src/tests/Search.test.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import Search from '../components/Search'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import { CountriesProvider } from '../context/countriesContext'
 import { GetTestById } from '../types/Tests.interface'
 let getTestById: GetTestById
 
 beforeEach(() => {
-   const component = render(<Search />)
+   const component = render(
+      <CountriesProvider>
+         <Search />
+      </CountriesProvider>
+   )
    getTestById = component.getByTestId
 })
 
-test('search input value updates on change', async () => {
+test('search input value updates on change', () => {
    const search = getTestById('search-input') as HTMLInputElement
    expect(search.value).toBe('')
 })
