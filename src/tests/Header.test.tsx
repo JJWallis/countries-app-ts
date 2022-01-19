1b522199b9e79d98ff3230052c4c18a76ec54afd
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-19 15:12:12 +0000

    HEader tests beforeEach added

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Header from '../components/Header'
-import { render, fireEvent } from '@testing-library/react'
+import { render, Matcher, MatcherOptions } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+let getTestById: (
+   text: Matcher,
+   options?: MatcherOptions | undefined,
+   waitForElementOptions?: unknown
+) => HTMLElement
 
-test('header title renders with correct text', () => {
-   const { getByTestId } = render(
+beforeEach(() => {
+   const component = render(
       <Header handleThemeChange={(dark: boolean) => undefined} />
    )
-   const title = getByTestId('header-title')
+   getTestById = component.getByTestId
+})
+
+test('header title renders with correct text', () => {
+   const title = getTestById('header-title')
    expect(title).toHaveTextContent('Where in the world?')
 })
