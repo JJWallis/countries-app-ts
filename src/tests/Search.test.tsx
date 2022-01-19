5f7ba78f4cf0b6124665d7ae7edbb7e89d3e3ad7
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-19 16:06:59 +0000

    Search fireEvent "leaking" bug ip

--- src/tests/Search.test.tsx
+++ src/tests/Search.test.tsx
@@ -5,6 +5,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { CountriesProvider } from '../context/countriesContext'
 import { BrowserRouter } from 'react-router-dom'
 import { GetTestById } from '../types/Tests.interface'
+import { act } from 'react-dom/test-utils'
 let getTestById: GetTestById
 
 beforeEach(() => {
@@ -18,9 +19,20 @@ beforeEach(() => {
    getTestById = component.getByTestId
 })
 
-test('search input value updates on change', () => {
+test('input value updates on change', () => {
    const search = getTestById('search-input') as HTMLInputElement
    expect(search.value).toBe('')
    fireEvent.change(search, { target: { value: 'test' } })
    expect(search.value).toBe('test')
+})
+
+test('input error styles update on invalid search', () => {
+   const search = getTestById('search-input') as HTMLInputElement
+   const searchIcon = getTestById('search-icon')
+   expect(search.value).toBe('')
+   fireEvent.change(search, { target: { value: 'test' } })
+   act(() => {
+      fireEvent.click(searchIcon)
+   })
+   expect(search.placeholder).toBe('Please enter a valid country')
 })
