879122315a3c2bae543bd1c45e786ce7b24e2298
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-25 16:36:48 +0000

    Back Button test setup

---
+++ src/tests/BackButton.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '@testing-library/jest-dom/extend-expect'
+
+test('back button renders correctly', () => {
+   screen.getByRole('')
+})
