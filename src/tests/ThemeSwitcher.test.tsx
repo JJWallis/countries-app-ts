d4ddd9ab97e5aadc43a83339de0b76e217e7edea
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-25 18:02:09 +0000

    ThemeSwitcher test setup ip

--- src/tests/ThemeSwitcher.test.tsx
+++ src/tests/ThemeSwitcher.test.tsx
@@ -4,10 +4,10 @@ import '@testing-library/jest-dom/extend-expect'
 import { CountriesProvider } from '../context/countriesContext'
 import ThemeSwitcher from '../components/ThemeSwitcher'
 import { BrowserRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 
-beforeEach(() => {
+test('handleThemeChange callback executed', async () => {
    const themeChange = jest.fn()
-
    render(
       <CountriesProvider>
          <BrowserRouter>
@@ -15,4 +15,9 @@ beforeEach(() => {
          </BrowserRouter>
       </CountriesProvider>
    )
+
+   const checkbox = screen.getByRole('checkbox')
+   userEvent.click(checkbox)
+   expect(checkbox).toBeChecked()
+   expect(themeChange).toHaveBeenCalledTimes(1)
 })
