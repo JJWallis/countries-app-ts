fbd68065d7a7ece98f4d7512d91ef2d370916b90
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-31 17:08:04 +0000

    HomeMain axios mock test failing for rejected promise

--- src/tests/HomeMain.test.tsx
+++ src/tests/HomeMain.test.tsx
@@ -12,14 +12,11 @@ import {
 } from '../types/countriesContext.interface'
 let data: CountryData
 
-// expect(items).toHaveLength(250) - country cards
-
 jest.mock('axios')
 
 describe('loading title', () => {
    test('displays on initial render and disappears on successful data fetch', async () => {
       const axiosRequest = axios as jest.Mocked<typeof axios>
-
       axiosRequest.get.mockImplementationOnce(() =>
          Promise.resolve({ data: CountryMockTest })
       )
@@ -39,5 +36,31 @@ describe('loading title', () => {
 
       expect(screen.getByRole('heading')).toBeInTheDocument()
       expect(await screen.findByRole('heading')).not.toBeInTheDocument()
+      expect(await screen.findAllByRole('link')).toHaveLength(1)
+   })
+
+   test('displays error on unsuccessful data fetch', async () => {
+      const axiosReq = axios as jest.Mocked<typeof axios>
+      axiosReq.get.mockImplementationOnce(() => Promise.reject(new Error()))
+
+      render(
+         <BrowserRouter>
+            <CountriesProvider>
+               <FilteredRegionsProvider
+                  filteredRegions={data}
+                  handleFilterRegions={(region: string) => undefined}
+               >
+                  <HomeMain />
+               </FilteredRegionsProvider>
+            </CountriesProvider>
+         </BrowserRouter>
+      )
+
+      expect(screen.getByRole('heading')).toBeInTheDocument()
+      expect(await screen.findByRole('heading')).toHaveTextContent(
+         'Country data could not be retrieved. Please reload & try again.'
+      )
    })
 })
+
+// test() - if user clicks on card with 'hi' - fallbacks shown as no 'hi' country
