32299fc4aeec8108569edb4fd0fd36edff97e49e
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-03 17:15:15 +0000

    HomeMain test - failed fetch test ip

--- src/tests/HomeMain.test.tsx
+++ src/tests/HomeMain.test.tsx
@@ -20,17 +20,16 @@ describe('loading title', () => {
       screen.debug()
    })
 
-   //    test('displays error on unsuccessful data fetch', async () => {
-   //       const axiosReq = axios as jest.Mocked<typeof axios>
-   //       axiosReq.get.mockImplementationOnce(() => Promise.reject(new Error()))
-
-   //       render(<HomeMain />)
-
-   //       expect(screen.getByRole('heading')).toBeInTheDocument()
-   //       expect(await screen.findByRole('heading')).toHaveTextContent(
-   //          'Country data could not be retrieved. Please reload & try again.'
-   //       )
-   //    })
+   test('displays error on unsuccessful data fetch', async () => {
+      const axiosReq = axios as jest.Mocked<typeof axios>
+      axiosReq.get.mockImplementationOnce(() => Promise.reject(new Error()))
+      render(<HomeMain />)
+      screen.debug()
+      // expect(screen.getByRole('heading')).toBeInTheDocument()
+      // expect(await screen.findByRole('heading')).toHaveTextContent(
+      //    'Country data could not be retrieved. Please reload & try again.'
+      // )
+   })
 })
 
 // test() - if user clicks on card with 'hi' - fallbacks shown as no 'hi' country
