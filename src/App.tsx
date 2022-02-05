ab2d2918a0990a4b04a034bf6106b0f2311831cf
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-05 14:16:01 +0000

    countries state refactor ip - housing in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React, { useState, FC } from 'react'
+import React, { useState, FC, useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
+import { CountriesProvider } from './context/countriesContext'
 import { useDarkMode } from './hooks/useDarkMode'
 import { darkTheme, lightTheme } from './components/styled/Theme'
+import { CountryData } from './types/countriesContext.interface'
+import axios from 'axios'
 import GlobalStyles from './components/styled/GlobalStyles'
 import Header from './components/Header'
 import Main from './components/Main'
@@ -11,15 +14,34 @@ const App: FC = () => {
    const [theme, setTheme] = useState(() =>
       prefersDarkMode ? darkTheme : lightTheme
    )
+   const [countries, setCountries] = useState<CountryData>(null)
+   const [countriesError, setCountriesError] = useState(false)
 
    const handleThemeChange = (dark: boolean) =>
       dark ? setTheme(darkTheme) : setTheme(lightTheme)
 
+   function fetchData(endpoint: string) {
+      axios
+         .get<CountryData>(endpoint)
+         .then((value) => {
+            setCountries(value.data)
+            console.log(value.data)
+         })
+         .catch((err) => {
+            setCountriesError(true)
+            console.error(err.message)
+         })
+   }
+
+   useEffect(() => fetchData('https://restcountries.com/v3.1/all'), [])
+
    return (
       <ThemeProvider theme={theme}>
-         <GlobalStyles />
-         <Header handleThemeChange={handleThemeChange} />
-         <Main />
+         <CountriesProvider>
+            <GlobalStyles />
+            <Header handleThemeChange={handleThemeChange} />
+            <Main />
+         </CountriesProvider>
       </ThemeProvider>
    )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { CountriesProvider } from './context/countriesContext'
 
 ReactDOM.render(
    <React.StrictMode>
-      <CountriesProvider>
-         <BrowserRouter>
-            <Routes>
-               <Route path="/*" element={<App />} />
-            </Routes>
-         </BrowserRouter>
-      </CountriesProvider>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/*" element={<App />} />
+         </Routes>
+      </BrowserRouter>
    </React.StrictMode>,
    document.getElementById('root')
 )
