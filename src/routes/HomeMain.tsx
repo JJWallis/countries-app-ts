0f77534ee0225444353ae94388efeaa412305bd6
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-09 15:46:47 +0000

    key prop uuid added in HomeMain

--- src/routes/HomeMain.tsx
+++ src/routes/HomeMain.tsx
@@ -3,17 +3,18 @@ import { useCountriesContext } from '../hooks/useCountriesContext'
 import { useFilteredRegionsContext } from '../hooks/useFilteredRegionsContext'
 import { CountryContainer as Countries } from '../components/containers/CountryContainer.styled'
 import { Loading } from '../components/styled/StyledTitle'
+import { v4 as uuid } from 'uuid'
 import CountryCard from '../components/CountryCard'
 
 const HomeMain: React.FC = () => {
    const { countries, countriesError } = useCountriesContext()
    const filteredRegions = useFilteredRegionsContext()
-   const data = !filteredRegions ? countries : filteredRegions
+   const dataToRender = !filteredRegions ? countries : filteredRegions
 
-   return data ? (
+   return dataToRender ? (
       <Countries id="country-information">
-         {data.map((country, idx) => (
-            <CountryCard key={idx} data={country} />
+         {dataToRender.map((country) => (
+            <CountryCard key={uuid()} data={country} />
          ))}
       </Countries>
    ) : (
