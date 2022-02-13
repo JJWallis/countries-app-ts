1e03a748cc42006bce0bf6905ffc512db0a2dd5f
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-13 18:44:21 +0000

    backBtn renders correctly - test

--- src/tests/BackButton.test.tsx
+++ src/tests/BackButton.test.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import userEvent from '@testing-library/user-event'
-import { render, screen } from '@testing-library/react'
+import { render, screen } from './test-utils'
 import { Link } from 'react-router-dom'
 import '@testing-library/jest-dom/extend-expect'
+import BackButton from '../components/BackButton'
 
-test('back button renders correctly', () => {
-   //    screen.getByRole('')
-   // userEvent.click(screen.getByRole('button'))
+test('back button renders semantically', () => {
+   render(<BackButton />)
+   const link = screen.getByRole('link')
+   const backBtn = screen.getByRole('button')
+   expect(link).toHaveAttribute('href', '/')
+   screen.getByRole('')
 })
--- src/tests/BorderCountries.test.tsx
+++ src/tests/BorderCountries.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import userEvent from '@testing-library/user-event'
-import { render, screen } from '@testing-library/react'
+import { render, screen } from './test-utils'
 import { Link } from 'react-router-dom'
 import '@testing-library/jest-dom/extend-expect'
 
