cba1e7ed510af0871c1dee631b173f756267aa03
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-13 18:09:53 +0000

    furtherMain test setup

---
+++ src/tests/FurtherMain.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
+import { Link } from 'react-router-dom'
+import '@testing-library/jest-dom/extend-expect'
+
+test('', () => {})
