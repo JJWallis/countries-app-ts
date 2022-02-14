0b36132792d87e269e6bac5fa9d703ec6b2bbdb2
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-14 16:47:33 +0000

    search test refactored - destructered from render()

--- src/tests/Search.test.tsx
+++ src/tests/Search.test.tsx
@@ -3,31 +3,28 @@ import React from 'react'
 import { render, screen } from './test-utils'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect'
-import { GetByRole, GetTestById } from '../types/Tests.interface'
-let getTestById: GetTestById
-let getElByRole: GetByRole
-
-beforeEach(() => {
-   const component = render(<Search />)
-   getTestById = component.getByTestId
-   getElByRole = component.getByRole
-})
 
 test('is rendered correctly', () => {
-   const search = getElByRole('textbox')
+   const { getByRole } = render(<Search />)
+   const search = getByRole('textbox')
+
    expect(search).toBeTruthy()
 })
 
 test('input value updates on change', () => {
-   const search = getTestById('search-input') as HTMLInputElement
+   const { getByTestId } = render(<Search />)
+   const search = getByTestId('search-input') as HTMLInputElement
+
    expect(search.value).toBe('')
    userEvent.type(search, 'test')
    expect(screen.getByRole('textbox')).toHaveValue('test')
 })
 
 test('input error styles update on invalid search', () => {
-   const search = getTestById('search-input') as HTMLInputElement
-   const searchIcon = getTestById('search-icon')
+   const { getByTestId } = render(<Search />)
+   const search = getByTestId('search-input') as HTMLInputElement
+   const searchIcon = getByTestId('search-icon')
+
    expect(search.value).toBe('')
    userEvent.type(search, 'test')
    userEvent.click(searchIcon)
