64c791dff540c9520b645fc14f018c5c8eb9c623
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-14 16:38:28 +0000

    borderCountries initial test refactored - destructered from render()

--- src/tests/BorderCountries.test.tsx
+++ src/tests/BorderCountries.test.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { render, screen } from './test-utils'
+import { render } from './test-utils'
 import BorderCountries from '../components/BorderCountries'
 import { CountryMockTest } from '../types/countriesContext.interface'
 import '@testing-library/jest-dom/extend-expect'
 
 test('markup renders with valid country', () => {
-   render(<BorderCountries country={CountryMockTest[0]} />)
-   const link = screen.getByRole('link', { name: 'hi' })
-   const button = screen.getByRole('button', { name: 'hi' })
+   const { getAllByRole, getByRole, getByText } = render(
+      <BorderCountries country={CountryMockTest[0]} />
+   )
+   const link = getByRole('link', { name: 'hi' })
+   const button = getByRole('button', { name: 'hi' })
 
    expect(link).toHaveAttribute('href', '/details/hi')
-   expect(screen.getAllByRole('link')).toHaveLength(1)
+   expect(getAllByRole('link')).toHaveLength(1)
    expect(button).toHaveTextContent('hi')
    expect(button).toBeEnabled()
-   expect(screen.getByText(/border countries/i)).toBeInTheDocument()
+   expect(getByText(/border countries/i)).toBeInTheDocument()
 
    // screen.getByRole('')
 })
