11545944655dc9097689be7bd02f1a44a10f1ada
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-18 17:42:32 +0000

    ThemeSwitcher content renders correctly

--- src/tests/ThemeSwitcher.test.tsx
+++ src/tests/ThemeSwitcher.test.tsx
@@ -4,7 +4,24 @@ import '@testing-library/jest-dom/extend-expect'
 import ThemeSwitcher from '../components/ThemeSwitcher'
 import userEvent from '@testing-library/user-event'
 
-test('handleThemeChange callback executed', () => {
+test('HTML renders correctly & semantically', () => {
+   const themeChange = jest.fn()
+   const { getByRole, getByText, getByTestId } = render(
+      <ThemeSwitcher handleThemeChange={themeChange} />
+   )
+   const checkbox = getByRole('checkbox')
+   const label = getByText(/dark mode/i)
+   const icon = getByTestId('moon-icon')
+
+   expect(label).toBeInTheDocument()
+   expect(icon).toBeInTheDocument()
+   expect(checkbox).toBeInTheDocument()
+   expect(icon).toHaveAttribute('aria-hidden', 'true')
+
+   getByRole('')
+})
+
+test('handleThemeChange callback executed on theme change', () => {
    const themeChange = jest.fn()
    const { getByRole } = render(
       <ThemeSwitcher handleThemeChange={themeChange} />
