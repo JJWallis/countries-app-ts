0f8a583d0a228eea58f5d05fda4b9851c14c0c59
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-19 15:23:37 +0000

    filter test - all buttons render correctly

--- src/tests/Filter.test.tsx
+++ src/tests/Filter.test.tsx
@@ -6,7 +6,22 @@ import '@testing-library/jest-dom/extend-expect'
 
 test('HTML renders correctly & semantically', () => {
    const prevFilter = jest.fn()
-   const { getByRole } = render(
+   const { getByRole, getAllByRole } = render(
       <Filter prevFilter="" updatePrevFilter={prevFilter} />
    )
+   const btnTitle = getByRole('button', { name: /Filter by region/i })
+   const btnOption = getByRole('button', { name: /hi/i })
+   const btns = getAllByRole('button')
+
+   expect(btns).toHaveLength(2)
+   expect(btns.map((button) => button.getAttribute('aria-controls'))).toEqual([
+      'country-information',
+      'country-information',
+   ])
+
+   expect(btnTitle).toBeInTheDocument()
+   expect(btnOption).toBeInTheDocument()
+   expect(btnOption).toHaveAttribute('aria-selected', 'false')
+
+   getByRole('')
 })
