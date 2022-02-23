774c15ba46494a56b212fec447aaf9ceb2bc44d8
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-23 20:17:45 +0000

    countries container test ip

--- src/tests/CountriesContainer.test.tsx
+++ src/tests/CountriesContainer.test.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import userEvent from '@testing-library/user-event'
-import { render, screen } from './test-utils'
-import { Link } from 'react-router-dom'
+import { countriesRender } from './test-utils'
 import CountriesContainer from '../components/CountriesContainer'
+import axios from 'axios'
+import { CountryMockTest } from '../types/countriesContext.interface'
 import '@testing-library/jest-dom/extend-expect'
 
-test('initial test', () => {
-   const { getByRole } = render(<CountriesContainer />)
+jest.mock('axios')
+
+test('initial test', async () => {
+   const axiosRequest = axios as jest.Mocked<typeof axios>
+   axiosRequest.get.mockResolvedValue(() =>
+      Promise.resolve({ data: CountryMockTest })
+   )
+   const providerProps = {
+      countries: null,
+      countriesError: false,
+   }
+
+   const { findByRole } = countriesRender(<CountriesContainer />, {
+      providerProps,
+   })
+
+   await findByRole('')
 })
