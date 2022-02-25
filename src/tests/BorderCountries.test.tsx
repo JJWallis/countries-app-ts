01780ff752f84749e1d4844041cb9ae7989ded7c
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-25 13:59:15 +0000

    borderCountries test fixed with new mocked data

--- src/tests/BorderCountries.test.tsx
+++ src/tests/BorderCountries.test.tsx
@@ -11,29 +11,32 @@ test('HTML renders correctly & semantically', () => {
       countriesError: false,
    }
    const borders = country.borders.map((border) => border)
-   const { getAllByRole, getByRole, getByText, getAllByTestId } =
-      countriesRender(<BorderCountries country={country} />, { providerProps })
-   const link = getByRole('link', { name: 'hi' })
-   const button = getByRole('button', { name: 'hi' })
-   const btns = getAllByTestId('border-btn').map(
-      (btn) => btn.textContent
-   ) as string[]
+   const borderTotal = borders.length
+   const { getAllByRole, getByText, getAllByTestId } = countriesRender(
+      <BorderCountries country={country} />,
+      { providerProps }
+   )
+   const links = getAllByRole('link')
+   const btns = getAllByTestId('border-btn')
 
-   expect(link).toBeInTheDocument()
-   expect(button).toBeInTheDocument()
    expect(getByText(/border countries/i)).toBeInTheDocument()
 
-   expect(link).toHaveAttribute('href', '/details/hi')
-   expect(getAllByRole('link')).toHaveLength(1)
+   expect(links).toHaveLength(borderTotal)
+   expect(btns).toHaveLength(borderTotal)
 
-   expect(button).toBeEnabled()
-   expect(button).toHaveTextContent('hi')
+   links.map((link) =>
+      expect(link).toHaveAttribute('href', '/details/undefined')
+   )
 
-   expect(borders).toEqual(btns)
+   btns.map((btn) => {
+      expect(btn).toBeEnabled()
+      expect(btn).toHaveTextContent('')
+      return null
+   })
 })
 
 test('fallback button renders with undefined country', () => {
-   const { getByText, queryByRole, getAllByRole } = render(
+   const { getByText, queryByRole, getAllByRole, getByRole } = render(
       <BorderCountries country={CountryMockTest} />
    )
    const fallbackBtn = getByText(/no bordering countries/i)
