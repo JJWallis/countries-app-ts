0cfa0c77bd73314934afbeacad5f17090a5aff3c
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-27 09:37:35 +0000

    furtherDetails test removed

--- src/tests/FurtherDetails.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import userEvent from '@testing-library/user-event'
-import { render, screen } from '@testing-library/react'
-import { Link } from 'react-router-dom'
-import '@testing-library/jest-dom/extend-expect'
-
-test('', () => {})
