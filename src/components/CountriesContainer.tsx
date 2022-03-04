5ef81b7ad89c712def58a28960353f89e9beda81
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-04 18:51:58 +0000

    fallback component provided

--- src/components/CountriesContainer.tsx
+++ src/components/CountriesContainer.tsx
@@ -3,15 +3,18 @@ import { Route, Routes } from 'react-router-dom'
 import HomeMain from '../routes/HomeMain'
 import FurtherMain from '../routes/FurtherMain'
 import ErrorFallback from './ErrorFallback'
+import { ErrorBoundary } from 'react-error-boundary'
 
 const CountriesContainer: FC = () => {
    return (
       <>
-         <Routes>
-            <Route path="/" element={<HomeMain />} />
-            <Route path="/details/:country" element={<FurtherMain />} />
-            <Route path="*" element={<ErrorFallback />} />
-         </Routes>
+         <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <Routes>
+               <Route path="/" element={<HomeMain />} />
+               <Route path="/details/:country" element={<FurtherMain />} />
+               <Route path="*" element={<ErrorFallback />} />
+            </Routes>
+         </ErrorBoundary>
       </>
    )
 }
