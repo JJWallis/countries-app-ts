3bbb4b9d9e145901fd8051568f0f0266f13f341f
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-01 16:53:08 +0100

    DropDownCt comp added

---
+++ src/components/DropDownCt.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { DropDownContainer } from './containers/DropDownContainer.styled'
+
+interface Props {
+   testId: string
+   toggled: boolean
+   styleProp: 'filter' | 'search'
+}
+
+const DropDownCt: React.FC<Props> = ({
+   children,
+   testId,
+   toggled,
+   styleProp,
+}) => {
+   return (
+      <DropDownContainer
+         styleProp={styleProp}
+         opacity={toggled ? 1 : 0}
+         data-testid={testId}
+      >
+         {children}
+      </DropDownContainer>
+   )
+}
+
+// change drop down ct styles
+
+export default DropDownCt
