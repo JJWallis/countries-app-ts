8b61a0c207f23c48e06f9c7fbb2c654f281af818
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-02 16:49:09 +0100

    dropDown padding added and pre dynamic ref map for each link focus

--- src/components/styled/StyledLink.tsx
+++ src/components/styled/StyledLink.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 export const DropDownResult = styled(Link)`
    display: block;
    width: 100%;
+   padding-block: 0.5rem;
    &:hover {
       text-decoration: underline;
    }
