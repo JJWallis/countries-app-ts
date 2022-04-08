9c8cfbd476d0d80d89bd6d8bab5d0ad82358399d
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-08 17:02:03 +0100

    drop down ct - comp rendered in initial test

--- src/tests/DropDownCt.test.tsx
+++ src/tests/DropDownCt.test.tsx
@@ -2,4 +2,9 @@ import React from 'react'
 import { render } from './test-utils'
 import DropDownCt from '../components/DropDownCt'
 
-test('html renders semantically and correctly', () => {})
+test('html renders semantically and correctly', () => {
+   const { getByRole } = render(
+      <DropDownCt testId="drop-down-ct" toggled={false} styleProp="filter" />
+   )
+   getByRole('')
+})
