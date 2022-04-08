e4d1c9728bbfe91a45238f8686badb573abdd56d
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-08 17:09:29 +0100

    drop down ct opacity visible with diff props

--- src/tests/DropDownCt.test.tsx
+++ src/tests/DropDownCt.test.tsx
@@ -4,13 +4,21 @@ import DropDownCt from '../components/DropDownCt'
 import '@testing-library/jest-dom/extend-expect'
 
 test('drop down container renders invisible on mount', () => {
-   const { getByRole, getByTestId } = render(
+   const { getByTestId } = render(
       <DropDownCt testId="drop-down-ct" toggled={false} styleProp="filter" />
    )
    const container = getByTestId('drop-down-ct')
 
    expect(container).toBeInTheDocument()
    expect(container).toHaveStyle('opacity: 0')
+})
 
-   getByRole('')
+test('drop down container renders visible with differen props', () => {
+   const { getByTestId } = render(
+      <DropDownCt testId="drop-down-ct" toggled={true} styleProp="filter" />
+   )
+   const container = getByTestId('drop-down-ct')
+
+   expect(container).toBeInTheDocument()
+   expect(container).toHaveStyle('opacity: 1')
 })
