da17d330e4000f80463fe1cd4496dace8fb8fcbb
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-08 17:05:02 +0100

    drop down ct selected and in document

--- src/tests/DropDownCt.test.tsx
+++ src/tests/DropDownCt.test.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { render } from './test-utils'
 import DropDownCt from '../components/DropDownCt'
+import '@testing-library/jest-dom/extend-expect'
 
 test('html renders semantically and correctly', () => {
-   const { getByRole } = render(
+   const { getByRole, getByTestId } = render(
       <DropDownCt testId="drop-down-ct" toggled={false} styleProp="filter" />
    )
+   const container = getByTestId('drop-down-ct')
+   expect(container).toBeInTheDocument()
    getByRole('')
 })
