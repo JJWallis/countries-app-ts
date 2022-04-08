cf5dfc95ca4d2ffee111686bf303f9a035d1870b
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-08 17:06:54 +0100

    drop down ct opacity invisible on mount

--- src/tests/DropDownCt.test.tsx
+++ src/tests/DropDownCt.test.tsx
@@ -3,11 +3,14 @@ import { render } from './test-utils'
 import DropDownCt from '../components/DropDownCt'
 import '@testing-library/jest-dom/extend-expect'
 
-test('html renders semantically and correctly', () => {
+test('drop down container renders invisible on mount', () => {
    const { getByRole, getByTestId } = render(
       <DropDownCt testId="drop-down-ct" toggled={false} styleProp="filter" />
    )
    const container = getByTestId('drop-down-ct')
+
    expect(container).toBeInTheDocument()
+   expect(container).toHaveStyle('opacity: 0')
+
    getByRole('')
 })
