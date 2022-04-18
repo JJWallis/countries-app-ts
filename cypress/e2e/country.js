792fb96e1b9995778800b76578eb1c8f65079e52
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-18 14:09:07 +0100

    Cypress files updated

---
+++ cypress/e2e/country.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+beforeEach(() => {
+   cy.visit('/')
+})
+
+describe('theme toggle', () => {
+   it('changes theme on click', () => {
+      cy.contains('Dark Mode')
+         .should('have.text', 'Dark Mode')
+         .should('have.css', 'color', 'rgb(17, 21, 23)')
+         .click()
+         .should('have.css', 'color', 'rgb(255, 255, 255)')
+   })
+})
