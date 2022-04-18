f9c263078d4b78e2f3fc75d436e11516208e7115
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-18 10:48:52 +0100

    cypress initial test added

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -1,1 +1,5 @@
 /// <reference types="cypress" />
+
+describe('Todo', () => {
+   it('should dab'), () => {}
+})
---
+++ cypress/integration/country.js
@@ -0,0 +1,5 @@
+/// <reference types="cypress" />
+
+describe('intial test', () => {
+   it('does nothing', () => {})
+})
