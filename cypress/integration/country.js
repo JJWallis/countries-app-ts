bee74fc8c013001b623f0f294f24b536b95369f3
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-18 13:13:40 +0100

    dark mode label text correct

--- cypress/integration/country.js
+++ cypress/integration/country.js
@@ -1,12 +1,13 @@
 /// <reference types="cypress" />
 
 beforeEach(() => {
-   cy.visit('')
+   cy.visit('/')
 })
 
 describe('theme toggle', () => {
    it('changes theme on click', () => {
       cy.get('.sc-iJuVqt')
+         .should('have.text', 'Dark Mode')
          .should('have.css', 'color', 'rgb(17, 21, 23)')
          .click()
          .should('have.css', 'color', 'rgb(255, 255, 255)')
